b80f2c1c6823db25b684f5472dbb57297f6b4c65
unknown <you@example.com>
2020-05-13 21:02:14 +0200

    130520 refac commit

--- exercici7/functions.js
+++ exercici7/functions.js
@@ -1,49 +1,59 @@
 function meses(){
     //recogida datos
-    let mesAny = parseInt(document.getElementById('mes').value);
+    let mesAny = document.getElementById('mes').value;
+    mesAny = parseInt(mesAny);
+
+    //respuesta
+    let texto;
     
+    //dias
+
+    let oddDays  = "tiene 31 d&iacute;as";
+    let evenDays = "tiene 30 d&iacute;as";
 
     //expression ==> colocamos contenedor datos
     switch (mesAny) {
         case 1:
-            document.getElementById("demo").innerHTML = (`Enero tiene 31 días`);
+            texto = (`Enero ${oddDays}`);
             break;
         case 2:
-            document.getElementById("demo").innerHTML = (`Febrero tiene 28 días`)
+            texto = (`Febrero tiene 28 días`);
             break;
         case 3:
-            document.getElementById("demo").innerHTML = (`Marzo tiene 31 días`)
+            texto = (`Marzo ${oddDays}`);
             break;
         case 4:
-            document.getElementById("demo").innerHTML = (`Abril tiene 30 días`)
+            texto = (`Abril ${evenDays}`);
             break;
         case 5:
-            document.getElementById("demo").innerHTML = (`Mayo tiene 31 días`)
+            texto = (`Mayo ${oddDays}`);
             break;
         case 6:
-            document.getElementById("demo").innerHTML = (`Junio tiene 30 días`)
+            texto = (`Junio ${evenDays}`);
             break;
         case 7:
-            document.getElementById("demo").innerHTML = (`Julio tiene 31 días`)
+            texto = (`Julio ${oddDays}`);
             break;
         case 8:
-            document.getElementById("demo").innerHTML = (`Agosto tiene 31 días`)
+            texto = (`Agosto ${oddDays}`);
             break;
         case 9:
-            document.getElementById("demo").innerHTML = (`Septiembre tiene 30 días`)
+            texto = (`Septiembre ${evenDays}`);
             break;
         case 10:
-            document.getElementById("demo").innerHTML = (`Octubre tiene 31 días`)
+            texto = (`Octubre ${oddDays}`);
             break;
         case 11:
-            document.getElementById("demo").innerHTML = (`Noviembre tiene 30 días`)
+            texto = (`Noviembre ${evenDays}`);
             break;
         case 12:
-            document.getElementById("demo").innerHTML = (`Diciembre tiene 31 días`)
+            texto = (`Diciembre ${oddDays}`);
             break;
     
-        default:
-            document.getElementById("demo").innerHTML = (`Eso no es un mes!`)
-            break;
+/*         default:
+            texto = (`Eso no es un mes!`);
+            break; */
     }
+
+    document.getElementById('demo').innerHTML = texto;
 }
--- exercici8/functions.js
+++ exercici8/functions.js
@@ -1,31 +1,95 @@
+/* function meses(){
+
+    // datos input
+    let mesAny = document.getElementById('mes').value;
+    mesAny = parseInt(mesAny);
+
+    //contenedor respuesta
+    let respuesta;
+
+    //dias
+    let oddDays = " tiene 31 d&iacuteas";
+    let evenDays = " tiene 30 d&iacuteas";
+    
+
+
+
+
+    (mesAny === 1) ? respuesta = (`Enero ${oddDays}`) :
+
+    (mesAny === 2) ? respuesta = (`Febrero puede tener 28 0 29 d&iacuteas`) :
+
+    (mesAny === 3) ? respuesta = (`Marzo ${oddDays}`) :
+
+    (mesAny === 4) ? respuesta = (`Abril ${evenDays}`) :
+
+    (mesAny === 5) ? respuesta = (`Mayo ${oddDays}`) :
+
+    (mesAny === 6) ? respuesta = (`Mayo ${evenDays}`) :
+
+    (mesAny === 7) ? respuesta = (`Julio ${oddDays}`) :
+
+    (mesAny === 8) ? respuesta = (`Agosto ${oddDays}`) :
+
+    (mesAny === 9) ? respuesta = (`Septiembre ${evenDays}`) :
+
+    (mesAny === 10) ? respuesta = (`Octubre ${oddDays}`) :
+
+    (mesAny === 11) ? respuesta = (`Noviembre ${evenDays}`) :
+    
+                      respuesta = (`Diciembre ${oddDays}`);
+
+    //relleno contenedor respuestas
+    document.getElementById('demo').innerHTML = respuesta;
+    
+} */
+
 function meses(){
 
-    let mesAny = parseInt(document.getElementById('mes').value);
+    //datos
+    // aportacion de Cristina
+    let mesAny = document.getElementById('mes').value;
+    mesAny = parseInt(mesAny);
 
+    //respuesta
+    //aportacion de Cristina
+    let respuesta;
 
-
-    (mesAny === 1) ? document.getElementById("demo").innerHTML = (`Enero tiene 31 d&iacuteas`) :
-
-    (mesAny === 2) ? document.getElementById("demo").innerHTML = (`Febrero puede tener 28 0 29 d&iacuteas`) :
-
-    (mesAny === 3) ? document.getElementById("demo").innerHTML = (`Marzo tiene 31 d&iacuteas`) :
-
-    (mesAny === 4) ? document.getElementById("demo").innerHTML = (`Abril tiene 30 d&iacuteas`) :
-
-    (mesAny === 5) ? document.getElementById("demo").innerHTML = (`Mayo tiene 31 d&iacuteas`) :
-
-    (mesAny === 6) ? document.getElementById("demo").innerHTML = (`Mayo tiene 30 d&iacuteas`) :
-
-    (mesAny === 7) ? document.getElementById("demo").innerHTML = (`Julio tiene 31 d&iacuteas`) :
-
-    (mesAny === 8) ? document.getElementById("demo").innerHTML = (`Agosto tiene 31 d&iacuteas`) :
-
-    (mesAny === 9) ? document.getElementById("demo").innerHTML = (`Septiembre tiene 30 d&iacuteas`) :
-
-    (mesAny === 10) ? document.getElementById("demo").innerHTML = (`Octubre tiene 31 d&iacuteas`) :
-
-    (mesAny === 11) ? document.getElementById("demo").innerHTML = (`Noviembre tiene 30 d&iacuteas`) :
+    //arr meses
+    // aportación de Isaac Bejarano
+    let meses = [
+		"gener",
+		"febrer",
+		"març",
+		"abril",
+		"maig",
+		"juny",
+		"juliol",
+		"agost",
+		"setembre",
+		"octubre",
+		"novembre",
+		"desembre"
+    ];
     
-                     document.getElementById("demo").innerHTML = (`Diciembre tiene 31 d&iacuteas`);
-    
+    //marcamos el indice de la arr -1 por inicio arr posicion 0
+    let shorterOne = `El mes de ${mese[mesAny - 1]} puede tener 28 o 29 d&iacute;as`;
+    let longOne    = `El mes de ${mese[mesAny - 1]} tiene 31 d&iacute;as`;
+    let shortOne   = `El mes de ${mese[mesAny - 1]} tiene 30 d&iacute;as`;
+     
+    //condicionales
+
+    if ( mesAny === 1 || mesAny === 3 || mesAny === 5 || mesAny === 7 || mesAny === 8 || mesAny === 10 || mesAny === 12){
+
+        respuesta = longOne;
+    }else if (mesAny === 4 || mesAny === 6 || mesAny === 9 || mesAny === 10){
+
+        respuesta = shortOne;
+    }else {
+
+        respuesta = shorterOne;
+    }
+
+    //aportacion de Cristina
+    document.getElementById('demo').innerHTML = respuesta;
 }
